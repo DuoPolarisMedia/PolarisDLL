66401b8c1247329acb3a839b5cb5e1d938dafff3
ZephyrGDDP <162458898+ZephyrGDDP@users.noreply.github.com>
2025-03-09 09:27:32 +0100

    Begun attempt at archive installation
    Testing if dxinstaller batch files can install .zip files, as installing dozens of DLLs is inefficient and causes errors.

--- installer.py
+++ installer.py
@@ -1,10 +1,12 @@
 import os
-installver=int(input("Which DLL collection do you want to install?\n1 = DirectX 9\n2 = DirectX 11\n3 = DirectX 12\n4 = VCRedist"))
+script_dir = os.path.dirname(os.path.abspath(__file__))
+util_dir = os.path.join(script_dir, "util")
+installver = int(input("Which DLL collection do you want to install?\n1 = DirectX 9\n2 = DirectX 11\n3 = DirectX 12\n4 = VCRedist"))
 if installver == 1:
-    os.startfile("util/dx9installer.bat")
+    os.startfile(os.path.join(util_dir, "dx9installer.bat"))
 elif installver == 2:
-    os.startfile("util/dx11installer.bat")
+    os.startfile(os.path.join(util_dir, "dx11installer.bat"))
 elif installver == 3:
-    os.startfile("util/dx12installer.bat")
+    os.startfile(os.path.join(util_dir, "dx12installer.bat"))
 elif installver == 4:
-    os.startfile("util/vcredistinstaller.bat")
+    os.startfile(os.path.join(util_dir, "vcredistinstaller.bat"))
