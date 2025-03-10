c34ac0460114fa7d85c0a14f858ff5e516575173
ZephyrGDDP <162458898+ZephyrGDDP@users.noreply.github.com>
2025-03-10 11:43:38 +0100

    Created single installer file

--- util/dx12installer.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set "zipurl=https://zephyrgddp.github.io/DLLlib/dlls/dx12.zip"
-echo Opening download link in Edge...
-start microsoft-edge:%zipurl%
--- util/dx9installer.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set "zipurl=https://zephyrgddp.github.io/DLLlib/dlls/dx9.zip"
-echo Opening download link in Edge...
-start microsoft-edge:%zipurl%
