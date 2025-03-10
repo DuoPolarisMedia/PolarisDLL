c3c02890cdcb75c5c1ac93f71ea81f85b05404ab
ZephyrGDDP <162458898+ZephyrGDDP@users.noreply.github.com>
2025-03-10 11:27:27 +0100

    Fixed dx9installer.bat

--- util/dx9installer.bat
+++ util/dx9installer.bat
@@ -1,4 +1,4 @@
 @echo off
-set "zipurl=https://zephyrgddp.github.io/DLLlibBeta/dlls/dx9.zip"
+set "zipurl=https://zephyrgddp.github.io/DLLlib/dlls/dx9.zip"
 echo Opening download link in Edge...
 start microsoft-edge:%zipurl%
